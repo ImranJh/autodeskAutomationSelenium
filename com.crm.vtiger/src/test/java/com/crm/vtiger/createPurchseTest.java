2f80861598fa9dfdb92c1e95a6dceff7505b21af
Hp <Hp@DESKTOP-5E614SC>
2022-03-23 23:15:01 +0530

    my second push

---
+++ com.crm.vtiger/src/test/java/com/crm/vtiger/createPurchseTest.java
@@ -0,0 +1,12 @@
+package com.crm.vtiger;
+
+import org.testng.annotations.Test;
+
+public class createPurchseTest {
+
+	@Test
+	public void purchase()
+	{
+		System.out.println("purchase created");
+	}
+}
