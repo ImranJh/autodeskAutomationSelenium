a83e3f40585df210e80e3514a8db12414ede7cc4
Hp <Hp@DESKTOP-5E614SC>
2022-03-23 23:20:16 +0530

    2nd Push

---
+++ com.crm.vtiger/src/test/java/com/crm/vtiger/DemoTest.java
@@ -0,0 +1,12 @@
+package com.crm.vtiger;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void salesOrder()
+	{
+		System.out.println("Sales order created");
+	}
+
+}
