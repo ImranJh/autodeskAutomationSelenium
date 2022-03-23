199b4396ccc219da30b4045ff68a7a8368d9d93c
Hp <Hp@DESKTOP-5E614SC>
2022-03-23 17:22:56 +0530

    my first push

---
+++ com.crm.vtiger/src/test/java/com/crm/vtiger/createContactTest.java
@@ -0,0 +1,18 @@
+package com.crm.vtiger;
+
+import org.testng.annotations.Test;
+
+public class createContactTest {
+	@Test
+	public void Contact()
+	{
+		System.out.println("Contact Created");
+	}
+
+	
+	@Test
+	public void Contact2()
+	{
+		System.out.println("Contact2 Created");
+	}
+}
