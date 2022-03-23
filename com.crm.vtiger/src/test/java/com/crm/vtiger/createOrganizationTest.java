6628121277196b745a3dbbf93d4d16b6796cfaaa
Hp <Hp@DESKTOP-5E614SC>
2022-03-23 23:05:38 +0530

    second push

---
+++ com.crm.vtiger/src/test/java/com/crm/vtiger/createOrganizationTest.java
@@ -0,0 +1,12 @@
+package com.crm.vtiger;
+
+import org.testng.annotations.Test;
+
+public class createOrganizationTest {
+	@Test
+	public void Organization()
+	{
+		System.out.println("Organization created");
+	}
+
+}
